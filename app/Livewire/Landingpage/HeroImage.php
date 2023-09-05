dd6e798829f678093c3510101e281c303434a322
chandra <candrasachio@gmail.com>
2023-09-06 02:03:49 +0700

    optimize

--- app/Livewire/Landingpage/HeroImage.php
+++ app/Livewire/Landingpage/HeroImage.php
@@ -4,15 +4,25 @@ namespace App\Livewire\Landingpage;
 
 use App\Models\ImageSlider;
 use Livewire\Component;
+use Illuminate\Support\Facades\Cache;
 
 class HeroImage extends Component
 {
     public function render()
     {
-        return view('livewire.landingpage.hero-image', [
+        $cacheKey = env('APP_KEY');
+
+        // Check if the cached content exists
+        if (Cache::has($cacheKey)) {
+            return Cache::get($cacheKey);
+        }
+        $view = view('livewire.landingpage.hero-image', [
             'image_sliders' => ImageSlider::where('status', 'Publish')
                 ->get()
-        ]);
+        ])->render();
+        Cache::put($cacheKey, $view, 600);
+
+        return $view;
     }
 } 
  
