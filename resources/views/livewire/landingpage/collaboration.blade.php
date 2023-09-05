3ee7555e55bbdf3e7103d0d5c8e02805bbe6482d
chandra <candrasachio@gmail.com>
2023-09-06 02:14:03 +0700

    fix logo

--- resources/views/livewire/landingpage/collaboration.blade.php
+++ resources/views/livewire/landingpage/collaboration.blade.php
@@ -6,31 +6,28 @@
                 <div class="col-lg-6 col-md-6 col-12 mt-10">
                     <div class="process-item">
                         <h4><a href="{{route('id.program.regular')}}">PROGRAM REGULAR</a></h4>
-                        <img src="{{ asset('asset_be/img/logo/logo-bosa.webp')}}" 
-                            srcset="{{ asset('asset_be/img/logo/logo-bosa.webp')}} 500w,
+                        <amp-img alt="Hummingbird" src="{{ asset('asset_be/img/logo/logo-bosa.webp')}}" width="640" height="457" layout="responsive" srcset="{{ asset('asset_be/img/logo/logo-bosa.webp')}} 640w,
+                        {{ asset('asset_be/img/logo/logo-bosa.webp')}} 320w">
+                        </amp-img>
+                        <img src="{{ asset('asset_be/img/logo/logo-bosa.webp')}}" srcset="{{ asset('asset_be/img/logo/logo-bosa.webp')}} 500w,
                             {{ asset('asset_be/img/logo/logo-bosa.webp')}} 1000w,
-                            {{ asset('asset_be/img/logo/logo-bosa.webp')}} 1500w" 
-                            sizes="(max-width: 600px) 300px,
+                            {{ asset('asset_be/img/logo/logo-bosa.webp')}} 1500w" sizes="(max-width: 600px) 300px,
                             (max-width: 1200px) 600px,
-                            1200px"
-                            alt="Logo Program Regular" 
-                            loading="lazy"
-                        >
+                            1200px" alt="Logo Program Regular" loading="lazy">
                     </div>
                 </div>
                 <div class="col-lg-6 col-md-6 col-12 mt-10">
                     <div class="process-item">
                         <h4><a href="{{route('id.program.bosa-ais')}}">BOSA-AIS EDUCATIONAL PROGRAM</a></h4>
+                        <amp-img alt="Hummingbird" src="{{ asset('asset_be/img/logo/logo-bosa-ais.webp')}}" width="640" height="457" layout="responsive" srcset="{{ asset('asset_be/img/logo/logo-bosa-ais.webp')}} 640w,
+                        {{ asset('asset_be/img/logo/logo-bosa-ais.webp')}} 320w">
+                        </amp-img>
                         <img src="{{ asset('asset_be/img/logo/logo-bosa-ais.webp')}}" srcset="{{ asset('asset_be/img/logo/logo-bosa-ais.webp')}} 500w,
                             {{ asset('asset_be/img/logo/logo-bosa-ais.webp')}} 1000w,
-                            {{ asset('asset_be/img/logo/logo-bosa-ais.webp')}} 1500w" 
-                            sizes="(max-width: 600px) 300px,
+                            {{ asset('asset_be/img/logo/logo-bosa-ais.webp')}} 1500w" sizes="(max-width: 600px) 300px,
                             (max-width: 1200px) 600px,
-                            1200px"
-                            alt="Logo Bosa Ais" 
-                            loading="lazy"
-                        />
-                        
+                            1200px" alt="Logo Bosa Ais" loading="lazy" />
+
                     </div>
                 </div>
             </div>
