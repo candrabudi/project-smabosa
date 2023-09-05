4be816e69ab34a372a6c8300b6e02ce4d0c1c5d4
chandra <candrasachio@gmail.com>
2023-09-06 02:31:20 +0700

    seo menu

--- resources/views/livewire/english/landingpage/hero-image.blade.php
+++ resources/views/livewire/english/landingpage/hero-image.blade.php
@@ -2,7 +2,7 @@
     @foreach($image_sliders as $slider)
     <div class="hero-container">
         <div class="hero-inner">
-            <img src="{{ asset('images_upload/'.$slider->image) }}" class="img-fluid" alt="{{$slider->title}}" x-ref="lazyImage">
+            <img src="{{ asset('images_upload/'.$slider->image) }}" class="img-fluid" alt="{{$slider->title}}" loading="lazy">
         </div>
     </div>
     @endforeach
